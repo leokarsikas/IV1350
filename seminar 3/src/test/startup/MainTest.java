d128831cf5b2bf06f1508d03286e6520b17e6fa5
nillof@kth.se <nillof@kth.se>
2024-05-01 14:28:31 +0200

    Update MainTest.java

--- seminar 3/src/test/startup/MainTest.java
+++ seminar 3/src/test/startup/MainTest.java
@@ -1,24 +1,11 @@
-package test.startup;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import se.kth.iv1350.pointOfSale.integration.Printer;
-
+package se.kth.iv1350.pointOfSale.startup;
 import org.junit.jupiter.api.Test;
+
 
 public class MainTest {
     @Test
     public void main() {
         System.out.println("Main");
         String[] args = null;
-    }
-
-    @Test 
-
-    public void testPrinter() {
-      Printer printer = new Printer();
-      String testOutput = "Test output";
-      printer.setOutput(testOutput);
-      assertEquals(testOutput, printer.getOutput());
-     
-    
     }
 }
