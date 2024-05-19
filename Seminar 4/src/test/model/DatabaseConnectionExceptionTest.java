8d30fa9832b2dc5f5b616d961d46e9e89136ae95
Leo Karsikas <leoka@kth.se>
2024-05-19 14:40:36 +0200

    test for DatabaseConnection

---
+++ Seminar 4/src/test/model/DatabaseConnectionExceptionTest.java
@@ -0,0 +1,23 @@
+package test.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import se.kth.iv1350.pointOfSale.exceptions.DatabaseConnectionException;
+
+public class DatabaseConnectionExceptionTest {
+
+    @Test
+    public void testErrorMessage() {
+        String errorMessage = "InventoryDatabaseFailure";
+        DatabaseConnectionException exception = new DatabaseConnectionException(errorMessage);
+        assertEquals("Database " + errorMessage + " is not responding.", exception.getMessage(), "Error message is incorrect");
+    }
+
+    @Test
+    public void testExceptionType() {
+        String errorMessage = "InventoryDatabaseFailure";
+        DatabaseConnectionException exception = new DatabaseConnectionException(errorMessage);
+        assertTrue(exception instanceof RuntimeException, "Exception type is incorrect");
+    }
+}
