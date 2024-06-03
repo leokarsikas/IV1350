cd702bf87354a0d215751a4f6e13e7220fa48efe
Olle <olle.fomin@hotmail.com>
2024-06-03 12:37:36 +0200

    Update SaleTest.java

--- seminar 3/src/test/se/kth/iv1350/pointOfSale/model/SaleTest.java
+++ seminar 3/src/test/se/kth/iv1350/pointOfSale/model/SaleTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import se.kth.iv1350.pointOfSale.model.Item;
-import se.kth.iv1350.pointOfSale.model.Sale;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import se.kth.iv1350.pointOfSale.DTO.ItemDTO;
 import se.kth.iv1350.pointOfSale.DTO.SaleLogDTO;
 import se.kth.iv1350.pointOfSale.integration.*;
-
 
 /**
  * Test the Sale class in the model package.
@@ -17,9 +17,8 @@ import se.kth.iv1350.pointOfSale.integration.*;
 public class SaleTest {
     private Sale sale;
 
-
     /*
-    Sets up an sale object before eact test iteration
+     * Sets up an sale object before eact test iteration
      */
     @BeforeEach
     public void setUp() {
@@ -28,30 +27,40 @@ public class SaleTest {
         sale = new Sale(inventorySystem, printer);
     }
 
-    /*abc123
-    Test for checking the running total is correct 
-    */
+    /*
+     * abc123
+     * Test for checking the running total is correct
+     */
     @Test
     public void testRunningTotal() {
         // Add some items
         sale.addItem("abc123");
         sale.addItem("abc123");
         sale.addItem("def456");
-        
+
         // Assert that the running total is correct
         assertEquals(74.7, sale.getRunningTotal(), "Running total is not correct");
     }
 
     /*
-    Tests if the change amount i correct 
-    */
+     * Tests if the change amount i correct
+     */
     @Test
-    public void calculateChange(){
+    public void testCalculateChange() {
         sale.addItem("abc123");
         sale.addItem("abc123");
         sale.addItem("def456");
 
-        assertEquals(25,Math.round(sale.calculateChange(100)), "Change amount is not correct");
+        assertEquals(25, Math.round(sale.calculateChange(100)), "Change amount is not correct");
+    }
+
+    @Test 
+    public void testSetTimeOfSale(){
+        sale.fetchSaleInfo().getTimeOfSale();
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm"); //Just for nice output
+
+        assertEquals(time.format(dateFormat), sale.fetchSaleInfo().getTimeOfSale().format(dateFormat), "time is not correct");
     }
 
     @Test
@@ -65,21 +74,17 @@ public class SaleTest {
         assertEquals(6.0, itemDTO.getVAT(), "Item VAT is not correct");
     }
 
-      @Test
+    @Test
     public void testIsItemAlreadyInSale() {
-       sale.addItem("abc123");
-       ItemDTO item = sale.addItem("abc123");
+        sale.addItem("abc123");
+        ItemDTO item = sale.addItem("abc123");
 
         SaleLogDTO saleLogDTO = sale.fetchSaleInfo();
 
-         
-
         assertEquals(1, saleLogDTO.getLength(), "Items in the array should be 1");
 
-        
         assertEquals(2, item.getQuantity(), "Item quantity should be 2");
 
-       
         assertEquals("abc123", saleLogDTO.getItemFromList(0).getID(), "Item ID should be abc123");
 
         assertEquals(null, saleLogDTO.getItemFromList(1), "There should be no items at index 1");
@@ -87,25 +92,25 @@ public class SaleTest {
 
     @Test
     public void testAddNewItem() {
-        sale.addNewItem("abc123");
+        sale.addItem("abc123");
+        sale.addItem("def456");
 
-        Item item = sale.getItems().get(0);  // Assuming you have a getter for items
-        assertNotNull(item, "Item should not be null");
-        assertEquals("abc123", item.getID(), "Item ID is not correct");
-        assertEquals(1, item.getQuantity(), "Item quantity should be 1");
+        SaleLogDTO saleLogDTO = sale.fetchSaleInfo(); 
+
+        assertNotNull(saleLogDTO.getItemFromList(0), "Item should not be null");
+        assertNotNull(saleLogDTO.getItemFromList(1), "Item should not be null");
+        assertEquals("abc123", saleLogDTO.getItemFromList(0).getID(), "Item ID is not correct");
+        assertEquals("def456", saleLogDTO.getItemFromList(1).getID(), "Item ID is not correct");
+        assertEquals(1, saleLogDTO.getItemFromList(0).getQuantity(), "Item quantity should be 1");
+        assertEquals(1, saleLogDTO.getItemFromList(1).getQuantity(), "Item quantity should be 1");
     }
 
-    
     @Test
     public void testUpdateSale() {
-        ItemDTO itemDTO = new ItemDTO("abc123", "TestItem", 10.0, "TestDescription", 1, 25.0);
-        sale.updateSale(itemDTO);
+       sale.addItem("abc123");
+       SaleLogDTO saleLogDTO = sale.fetchSaleInfo();
 
-        assertEquals(10.0, sale.getRunningTotal(), "Running total is not correct");
-        assertEquals(2.0, sale.getTotalVAT(), "Total VAT is not correct");
+        assertEquals(29.90, saleLogDTO.getRunningTotal(), "Running total is not correct");
+        assertEquals(1.6924528301886816, saleLogDTO.getTotalVAT(), "Total VAT is not correct");
     }
 }
-
-    
-
-
